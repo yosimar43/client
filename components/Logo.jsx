ef8aef4c124670f55a7982197fe620cdbb3552f5
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-10-31 13:15:00 -0500

    ui(home): Create logo component

---
+++ components/Logo.jsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <div className="rounded-full border-dotted border-yellow-400  duration-300 border-2 border-opacity-0  hover:border-opacity-90 flex justify-center p-3 w-14 h-14 duration-200 hover:-translate-y-2 ">
+      <span className="quickSand text-red-600 text-xl opacity-75  text-center hover:opacity-100 duration-200 ">
+        AWOS
+      </span>
+    </div>
+  );
+};
+
+export default Logo;
